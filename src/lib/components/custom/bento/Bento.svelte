879ca8ee548cdfa2c2275ba9f42a16a824de87a7
Garrett Foy <me@garrettfoy.com>
2025-06-16 14:58:11 -0500

    adjust to use dl, dt, dd

--- src/lib/components/custom/bento/Bento.svelte
+++ src/lib/components/custom/bento/Bento.svelte
@@ -13,20 +13,20 @@
     let { title, body, loading}: BentoProps = $props();
 </script>
 
-<div>
+<dl>
   <div class="my-4 w-full border-[0.5px] border-zinc-400 dark:border-white/70 relative shadow-[4px_4px_0px_0px_rgba(0,0,0)] dark:shadow-[3px_3px_0px_0px_rgba(255,255,255,0.7)]">
     <div class={cn("text-left", className)}>
       {#if loading}
         <Skeleton class="my-1 h-8 w-1/2" />
         <Skeleton class="my-1 h-8 w-3/4" />
       {:else}
-        <h2 class="text-lg font-bold mb-1 text-gray-900 dark:text-gray-100">
+        <dt class="text-lg font-bold mb-1 text-gray-900 dark:text-gray-100">
           {title}
-        </h2>
-        <p class="text-gray-700 dark:text-gray-300">
+        </dt>
+        <dd class="text-gray-700 dark:text-gray-300">
           {body}
-        </p>
+        </dd>
       {/if}
     </div>
   </div>
-</div>
+</dl>
